f4b851ad012595ec9a396ec4b00e4f66caebac13
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-01 22:16:26 -0300

    add swagger

--- src/main/java/com/example/pessoa/controllers/PessoaController.java
+++ src/main/java/com/example/pessoa/controllers/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,33 +18,43 @@ import com.example.pessoa.dto.PessoaDTO;
 import com.example.pessoa.dto.PessoaNoIdDTO;
 import com.example.pessoa.services.PessoaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping(value = "/api/pessoa")
+@RequestMapping(value = "/api")
+@Api(value="API REST PESSOA")
+@CrossOrigin(origins="*")
 public class PessoaController {
 	
 	@Autowired
 	private PessoaService service;
 	
 	@PostMapping
+	@ApiOperation(value ="Adiciona uma pessoa no banco em h2")
 	public ResponseEntity<PessoaNoIdDTO> insert(@RequestBody PessoaNoIdDTO pessoa) {
 		return ResponseEntity.ok(service.insert(pessoa));
 	}
 	@GetMapping
+	@ApiOperation(value = "Retorna todas as pessoas do banco")
 	public ResponseEntity<List<PessoaDTO>> findAll( ){
 		List<PessoaDTO> result = service.findAll();
 		return ResponseEntity.ok(result);
 	}
 	@GetMapping(value ="/{id}")
+	@ApiOperation(value = "Retorna a pessoa pelo id")
 	public ResponseEntity<PessoaNoIdDTO> findById(@PathVariable("id") Long id) {
 		return ResponseEntity.ok(service.findById(id));
 	}
 	
 	@PutMapping(value = "/{id}")
+	@ApiOperation(value = "Atualiza uma pessoa.")
 	public ResponseEntity<PessoaNoIdDTO> update(@PathVariable("id") Long id, @RequestBody PessoaNoIdDTO pessoa) throws Exception{
 		return ResponseEntity.ok(service.update(id, pessoa));
 	}
 	
 	@DeleteMapping(path = "/{id}")
+	@ApiOperation(value = "Deleta uma pessoa pelo id")
 	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
 		service.deleteById(id);
 		return ResponseEntity.ok(null);
