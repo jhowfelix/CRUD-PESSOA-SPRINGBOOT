4d85046ced5753448c35a96f3781cc52a2e3ca90
Jonathan Felix <jhow.wi56@gmail.com>
2021-12-01 22:21:18 -0300

    Finalizado

--- src/main/java/com/example/pessoa/config/SwaggerConfig.java
+++ src/main/java/com/example/pessoa/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	
 	@Bean
-    public Docket productApi() {
+    public Docket pessoaApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.pessoa"))
@@ -31,8 +31,8 @@ public class SwaggerConfig {
     private ApiInfo metaInfo() {
 
         ApiInfo apiInfo = new ApiInfo(
-                "Produtos API REST",
-                "API REST de cadastro de produtos.",
+                "Pessoa API REST",
+                "API REST de cadastro de pessoas.",
                 "1.0",
                 "Terms of Service",
                 new Contact("Jonathan Felix", "https://www.linkedin.com/in/jonathan-felix-12a9881a0/",
